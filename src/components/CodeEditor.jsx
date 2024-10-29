43cf32bf0edb29a54fc17ba7736bca43d4a95077
robinjohannesson2@gmail.com <robinjohannesson2@gmail.com>
2024-10-29 15:14:11 +0100

    Moved code editor component
    Committed by Robin Johannesson.

---
+++ src/components/CodeEditor.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useRef } from "react"
+import * as monaco from "monaco-editor"
+
+const CodeEditor = ({ onExecute }) => {
+    const editorRef = useRef(null)
+
+    useEffect(() => {
+        editorRef.current = monaco.editor.create(
+            document.getElementById("editor"),
+            {
+                value: "",
+                language: "javascript",
+                theme: "vs-dark",
+                automaticLayout: true,
+            }
+        )
+
+        return () => editorRef.current.dispose()
+    }, [])
+
+    const handleExecute = () => {
+        const code = editorRef.current.getValue()
+        onExecute(code)
+    }
+
+    return (
+        <div>
+            <div id="editor" style={{ height: "600px", width: "800px" }}></div>
+            <button onClick={handleExecute}>Exekvera kod</button>
+        </div>
+    )
+}
+
+export default CodeEditor
